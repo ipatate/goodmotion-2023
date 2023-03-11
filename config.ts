3c4b6cd5c75e803d51127f4dfd28ede719ac4639
ipatate <faramazpat@gmail.com>
2023-03-11 17:23:32 +0100

    WIP home

---
+++ config.ts
@@ -0,0 +1,5 @@
+export default {
+  baseline: "Des sites web rapides et éco-responsables !",
+  heroText:
+    "Développement de site web et d'applications web modernes.<br />Optimisation de la vitesse et réduction de l'impact environnemental des sites web, pour offrir une expérience utilisateur rapide et écologique.",
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,10 @@ module.exports = {
         secondary: "#F9C907",
       },
       backgroundImage: {
+        "hero-animated":
+          "linear-gradient(180deg, rgba(229,240,242,1) 0%, rgba(229,240,242,1) 51%, rgba(245,247,247,1) 100%);",
         home: "url('/assets/home.svg')",
+        "bg-home": "url('/assets/bg-hero.svg')",
       },
     },
   },
