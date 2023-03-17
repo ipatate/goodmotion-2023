175d831320ec849b5ade37da620a41067d1adeb6
ipatate <faramazpat@gmail.com>
2023-03-17 08:43:30 +0100

    add sitemap and test SITE_NAME var

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: "astro/assets/services/sharp",
+    service: "astro/assets/services/sharp"
   },
-  integrations: [tailwind(), mdx()],
+  site: 'https://www.goodmotion.fr',
+  integrations: [tailwind(), mdx(), sitemap()]
 });
